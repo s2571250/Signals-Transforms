4c3b7a9eb4907b3792a8f9ad21dfe30c30447e02
XanderHeij <73619224+XanderHeij@users.noreply.github.com>
2022-06-08 13:00:03 +0200

    main.py added

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def main():
     T = N/sr
     freq = n/T 
 
-    plt.stem(freq, np.abs(X))
-    # plt.stem(freq, np.abs(X), 'b', markerfmt=" ", basefmt="-b")
+    #plt.stem(freq, np.abs(X))
+    plt.stem(freq, np.abs(X), 'b', markerfmt=" ", basefmt="-b")
     plt.show()
-    
+
     plt.plot(t, ifft(X))
     plt.show()
 
